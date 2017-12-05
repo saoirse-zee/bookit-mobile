15c6345d8f22a40af300986b16ebcd7f949041cd
Ben Hernandez <ben.hernandez@wipro.com>
2017-12-05 11:27:18 -0700

    adding metric capture to unit tests

--- concourse/scripts/dependencies.sh
+++ concourse/scripts/dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 main () {
+  apt-get update && apt-get install -y jq && \
   mv dependency-cache/node_modules bookit-mobile && \
   cd bookit-mobile && \
   mv ./{.[!.],}* ../bookit-with-deps && \
--- concourse/scripts/run_unit_tests.sh
+++ concourse/scripts/run_unit_tests.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 main () {
-  cd bookit-with-deps && yarn test:unit
+  cd bookit-with-deps && \
+  yarn test:unit && \
+  rc=$?
+  yarn badge-coverage
+  cd ..
+  tar -zcf coverage/coverage.tar.gz ./bookit-with-deps/coverage
+  exit $rc
 }
 
 main
