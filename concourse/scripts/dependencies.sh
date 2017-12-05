b81ecc2de934ba0bc0839431feee8ce89e299b59
Ben Hernandez <ben.hernandez@wipro.com>
2017-12-05 11:42:56 -0700

    chmodding jq

--- concourse/scripts/dependencies.sh
+++ concourse/scripts/dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 main () {
-  curl -Lo /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+  curl -Lo /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 && \
+  chmod +x /usr/bin/jq && \
   mv dependency-cache/node_modules bookit-mobile && \
   cd bookit-mobile && \
   mv ./{.[!.],}* ../bookit-with-deps && \
