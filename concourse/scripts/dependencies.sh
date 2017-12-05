b46cce1ed34a14515a007d9ec128e17a1e9d60df
Ben Hernandez <ben.hernandez@wipro.com>
2017-12-05 11:39:46 -0700

    updating jq again

--- concourse/scripts/dependencies.sh
+++ concourse/scripts/dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 main () {
-  add-apt-repository ppa:eugenesan/ppa && apt-get update && apt-get install -y jq && \
+  curl -Lo /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
   mv dependency-cache/node_modules bookit-mobile && \
   cd bookit-mobile && \
   mv ./{.[!.],}* ../bookit-with-deps && \
