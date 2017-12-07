044d63d44fd912e91b8bde0611625058556f09a0
Ben Hernandez <ben.hernandez@wipro.com>
2017-12-07 09:23:03 -0700

    updating unit test history to show coverage %

--- concourse/scripts/helpers/update_unit_test_history.sh
+++ concourse/scripts/helpers/update_unit_test_history.sh
@@ -3,10 +3,9 @@
 main () {
   curl -Lo /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 && chmod +x /usr/bin/jq && \
   curl -Lo unit-tests.json "https://s3.amazonaws.com/bookit-mobile-artifacts/test-counts/unit-tests.json"
-  total_tests=$(jq '.numTotalTests' ./jest.json)
-  passing_tests=$(jq '.numPassedTests' ./jest.json)
-  time=$(jq '.startTime' ./jest.json)
-  jq ".labels += [$time] | .datasets[0].data += [$passing_tests] | .datasets[1].data += [$total_tests]" unit-tests.json >> unit-tests-temp.json
+  percent=$(printf "%0.2f\n" "$(npm run percent-covered -s)") #yarn stupidly has no way to run without logging
+  time=$(date +%s000)
+  jq ".labels += [$time] | .datasets[0].data += [$percent]" unit-tests.json >> unit-tests-temp.json
   mv unit-tests-temp.json unit-tests.json
 }
 
