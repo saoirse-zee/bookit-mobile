6f3a1eb1598b1b9c308b20dcf422f5e106b0749e
Z <12530451+saoirse-zee@users.noreply.github.com>
2017-12-15 11:02:24 -0700

    Add utility to tag bookables w their availability

---
+++ src/utils/getBookablesWithAvailability.js
@@ -0,0 +1,5 @@
+// const bookablesWithAvailability = getBookablesWithAvailability(bookingFormData, bookables)
+// const getBookablesWithAvailability = (bookingFormData, bookables) =>
+// bookables.map(bookable =>
+// bookable.bookings.reduce((booking, result) =>
+// bookingInterval.overlaps(bookingFormDataInterval) ? return false : result
