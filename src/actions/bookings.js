79405adf1f14728f10d49e4c6c3577dde026bdeb
Z <12530451+saoirse-zee@users.noreply.github.com>
2017-12-19 14:39:46 -0700

    Handle bookings request more intelligently

--- src/actions/bookings.js
+++ src/actions/bookings.js
@@ -1,7 +1,15 @@
 import { DateTime } from 'luxon'
 import api from '../api'
-import { RECEIVE_BOOKINGS } from './types'
+import {
+  REQUEST_BOOKINGS,
+  RECEIVE_BOOKINGS,
+  REQUEST_BOOKINGS_FAILED,
+} from './types'
 import { handleError } from '../utils'
+
+const requestBookings = () => ({
+  type: REQUEST_BOOKINGS,
+})
 
 const receiveBookings = json => ({
   type: RECEIVE_BOOKINGS,
@@ -9,8 +17,18 @@ const receiveBookings = json => ({
   receivedAt: DateTime.local(),
 })
 
+const requestBookingsFailed = () => ({
+  type: REQUEST_BOOKINGS_FAILED,
+})
+
 // eslint-disable-next-line import/prefer-default-export
 export const fetchBookings =
-  token => dispatch => api.fetchBookings(token)
-    .then(bookings => dispatch(receiveBookings(bookings)))
-    .catch(error => handleError(dispatch, error))
+  token => (dispatch) => {
+    requestBookings()
+    api.fetchBookings(token)
+      .then(bookings => dispatch(receiveBookings(bookings)))
+      .catch((error) => {
+        requestBookingsFailed()
+        handleError(dispatch, error)
+      })
+  }
